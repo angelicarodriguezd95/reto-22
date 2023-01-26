d7db76f0c38902564f56cd2d35552d4b1ceaddd7
Rosa Rodríguez <angierodriguezd95@gmail.com>
2023-01-25 19:37:00 -0500

    reto-22

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'asincrona21-angular'`, () => {
+  it(`should have as title 'asincrona22-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('asincrona21-angular');
+    expect(app.title).toEqual('asincrona22-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('asincrona21-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('asincrona22-angular app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'asincrona21-angular';
+  title = 'asincrona22-angular';
 }
